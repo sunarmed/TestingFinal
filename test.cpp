d09e3abc9f200d4d4e8c062472c0a14a217bcbec
noebouilly <noebouilly@hotmail.fr>
2018-12-23 15:22:38 +0100

    TODO 3 resolved, errors cleared

--- test.cpp
+++ test.cpp
@@ -16,27 +16,27 @@ void ATPG::test(void) {
 
   /* Fsim only mode */
   if(fsim_only)
-  {
-    fault_simulate_vectors(total_detect_num);
-    in_vector_no+=vectors.size();
-    display_undetect();
-    fprintf(stdout,"\n");
-    return;
-  }// if fsim only
+    {
+      fault_simulate_vectors(total_detect_num);
+      in_vector_no+=vectors.size();
+      display_undetect();
+      fprintf(stdout,"\n");
+      return;
+    }// if fsim only
   
   if(tdfsim_only)
-  {
-    transition_delay_fault_simulation(total_detect_num);
-    in_vector_no+=vectors.size();
-    display_undetect();
+    {
+      transition_delay_fault_simulation(total_detect_num);
+      in_vector_no+=vectors.size();
+      display_undetect();
   
-    printf("\n# Result:\n");
-    printf("-----------------------\n");
-    printf("# total transition delay faults: %d\n", num_of_tdf_fault);
-    printf("# total detected faults: %d\n", total_detect_num);
-    printf("# fault coverage: %lf %\n", (float)total_detect_num / (float)num_of_tdf_fault * 100);
-    return;
-  }// if fsim only
+      printf("\n# Result:\n");
+      printf("-----------------------\n");
+      printf("# total transition delay faults: %d\n", num_of_tdf_fault);
+      printf("# total detected faults: %d\n", total_detect_num);
+      printf("# fault coverage: %lf %\n", (float)total_detect_num / (float)num_of_tdf_fault * 100);
+      return;
+    }// if fsim only
 
 #if 1
   /* LOS ATPG of TDF */
@@ -44,82 +44,94 @@ void ATPG::test(void) {
   /* ATPG mode */
   /* Figure 5 in the PODEM paper */
   while(fault_under_test != nullptr  /* TODO 6: for all fault, repeat 1~5 */ ) {
-    // FOR TODO 6 // for(int i = 0; i < this->ndet; i++){
-
-
-  /* TODO 1 (DONE): take one fault from init_flist(use the generate_fault_list from PA3), generate the v2 pattern by PODEM (backtrack to PI and PPI) 
-	     STF <= STUCK1 , STR <= STUCK0 
-             podem(fault_under_test,current_backtracks)
-  */
     
-  int podem_state = podem(fault_under_test,current_backtracks);
-  for(j = 0; j < cktin.size(); j ++){
-    v2.push_back(cktin[j]->value);
-    v1.push_back(cktin[j]->value)
-  }
-  /* TODO 2: shift back one clock and mark the PI value*/
+    // FOR TODO 6 // for(int i = 0; i < this->ndet; i++){
+    
+    v1.clear();
+    v2.clear();
+    /* TODO 1 (DONE): take one fault from init_flist(use the generate_fault_list from PA3), generate the v2 pattern by PODEM (backtrack to PI and PPI) 
+       STF <= STUCK1 , STR <= STUCK0 
+       podem(fault_under_test,current_backtracks)
+    */
+    
+    int podem_state = podem(fault_under_test,current_backtracks);
+    for(j = 0; j < cktin.size(); j ++){
+      v2.push_back(cktin[j]->value);
+      v1.push_back(cktin[j]->value);
+    }
+    /* TODO 2: shift back one clock and mark the PI value*/
 
-  v1.insert(v1.begin(), U);
+    v1.insert(v1.begin(), U);
   
-  /* TODO 3: backtrack and generate v1 pattern (PI and PPI) */
+    /* TODO 3: backtrack and generate v1 pattern (PI and PPI) */
 
-  for(j = 0; j < cktin.size(); j ++){
-    cktin[j]->value = v1[j];
-  }
-  if(backward_imply(sort_wlist[fault_under_test->to_swlist], fault_under_test->fault_type) == TRUE){
-    v1[0] = cktin[0];
-  }
-  else{
-    // In case of backward_imply returns FLASE or CONFLICT, we'll see later
-    printf("backward_imply if FALSE or CONFLICT ... We don't know yet how te deal with it :\'(");
-  }
-  
-  /* TODO 3.5 Dynamic Test Compression */ 
+    for(j = 0; j < cktin.size(); j ++){
+      cktin[j]->value = v1[j];
+    }
+    if(backward_imply(sort_wlist[fault_under_test->to_swlist], fault_under_test->fault_type) == TRUE){
+      v1[0] = cktin[0]->value;
+    }
+    else{
+      // In case of backward_imply returns FLASE or CONFLICT, we'll see later
+      printf("backward_imply if FALSE or CONFLICT ... We don't know yet how te deal with it :\'(\n");
+    }
+    /* V1 and V2 printing for test */
+    printf("\n V1 = ");
+    for(int v: v1){
+      printf("%d", v);
+    }
+    printf("\n V2 = ");
+    for(int v: v2){
+      printf("%d", v);
+    }
+    printf("\n");
+    
+    /* TODO 3.5 Dynamic Test Compression */ 
 
 
     switch(  podem_state/* check if the test pattern is generated */  ) {
-      case TRUE:
-        /* form a vector */
-        vec.clear();
-        for (wptr w: cktin) {
-          vec.push_back(itoc(w->value));
-        }
-        /*by defect, we want only one pattern per fault */
-        /*run a fault simulation, drop ALL detected faults */
-        if (total_attempt_num == 1) {
+    case TRUE:
+      /* form a vector */
+      vec.clear();
+      for (wptr w: cktin) {
+	vec.push_back(itoc(w->value));
+      }
+      /*by defect, we want only one pattern per fault */
+      /*run a fault simulation, drop ALL detected faults */
+      if (total_attempt_num == 1) {
   
-/* TODO 4: check if other faults are detected by tdfsim() */
+	/* TODO 4: check if other faults are detected by tdfsim() */
 
-           fault_sim_a_vector(vec, current_detect_num);
-           total_detect_num += current_detect_num;
+	fault_sim_a_vector(vec, current_detect_num);
+	total_detect_num += current_detect_num;
 
-        }
-        /* If we want mutiple petterns per fault, 
-         * NO fault simulation.  drop ONLY the fault under test */ 
-        else {
+      }
+      /* If we want mutiple petterns per fault, 
+       * NO fault simulation.  drop ONLY the fault under test */ 
+      else {
 
-/* TODO 5 (DONE) : increase nb_of_detect, if nb_of_detect == ndet, remove fault from init_flist */
+	/* TODO 5 (DONE) : increase nb_of_detect, if nb_of_detect == ndet, remove fault from init_flist */
 	  
 
-          fault_under_test->detect = TRUE;
-	  fault_under_test->detected_time ++;
-          /* drop fault_under_test if it has bit detected the enough amount of times*/
-	  if (fault_under_test->detected_time >= this->ndet){
-	    flist_undetect.remove(fault_under_test);
-	  }
+	fault_under_test->detect = TRUE;
+	fault_under_test->detected_time ++;
+	/* drop fault_under_test if it has bit detected the enough amount of times*/
+	if (fault_under_test->detected_time >= this->ndet){
+	  flist_undetect.remove(fault_under_test);
+	}
 
 
-        }
-        in_vector_no++;
-        break;
-	  case FALSE:
-        fault_under_test->detect = REDUNDANT;
-        no_of_redundant_faults++;
-        break;
+      }
+      in_vector_no++;
+      break;
+    case FALSE:
+      fault_under_test->detect = REDUNDANT;
+      no_of_redundant_faults++;
+      break;
   
-	  case MAYBE:
-        no_of_aborted_faults++;
-        break;
+    case MAYBE:
+      no_of_aborted_faults++;
+      break;
     }
     fault_under_test->test_tried = true;
     fault_under_test = nullptr;
@@ -140,35 +152,35 @@ void ATPG::test(void) {
   /* Figure 5 in the PODEM paper */
   while(fault_under_test != nullptr) {
     switch(podem(fault_under_test,current_backtracks)) {
-      case TRUE:
-        /* form a vector */
-        vec.clear();
-        for (wptr w: cktin) {
-          vec.push_back(itoc(w->value));
-        }
-        /*by defect, we want only one pattern per fault */
-        /*run a fault simulation, drop ALL detected faults */
-        if (total_attempt_num == 1) {
-          fault_sim_a_vector(vec, current_detect_num);
-          total_detect_num += current_detect_num;
-        }
-        /* If we want mutiple petterns per fault, 
-         * NO fault simulation.  drop ONLY the fault under test */ 
-        else {
-          fault_under_test->detect = TRUE;
-          /* drop fault_under_test */
-          flist_undetect.remove(fault_under_test);
-        }
-        in_vector_no++;
-        break;
-	  case FALSE:
-        fault_under_test->detect = REDUNDANT;
-        no_of_redundant_faults++;
-        break;
+    case TRUE:
+      /* form a vector */
+      vec.clear();
+      for (wptr w: cktin) {
+	vec.push_back(itoc(w->value));
+      }
+      /*by defect, we want only one pattern per fault */
+      /*run a fault simulation, drop ALL detected faults */
+      if (total_attempt_num == 1) {
+	fault_sim_a_vector(vec, current_detect_num);
+	total_detect_num += current_detect_num;
+      }
+      /* If we want mutiple petterns per fault, 
+       * NO fault simulation.  drop ONLY the fault under test */ 
+      else {
+	fault_under_test->detect = TRUE;
+	/* drop fault_under_test */
+	flist_undetect.remove(fault_under_test);
+      }
+      in_vector_no++;
+      break;
+    case FALSE:
+      fault_under_test->detect = REDUNDANT;
+      no_of_redundant_faults++;
+      break;
   
-	  case MAYBE:
-        no_of_aborted_faults++;
-        break;
+    case MAYBE:
+      no_of_aborted_faults++;
+      break;
     }
     fault_under_test->test_tried = true;
     fault_under_test = nullptr;
